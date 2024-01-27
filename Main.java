4193a9ea96a664ad1c5a05b4f89aee057bf6067f
Andrey <cliff_06@bk.ru>
2024-01-27 17:50:02 +0500

    создали код в файле Main

--- Main.java
+++ Main.java
@@ -1,5 +1,82 @@
+
+
+import java.util.*;
+
 public class Main {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
+        Set<Notebook> notebooks = new HashSet<>();
+        notebooks.add(new Notebook("Model1", 8, 512, "Windows 10", "Black"));
+        notebooks.add(new Notebook("Model2", 16, 1024, "MacOS", "Silver"));
+        notebooks.add(new Notebook("Model3", 4, 256, "Ubuntu", "White"));
+        notebooks.add(new Notebook("Model4", 12, 1000, "Windows 11", "Red"));
+        notebooks.add(new Notebook("Model5", 16, 512, "MacOS", "Blue"));
+        notebooks.add(new Notebook("Model6", 8, 1000, "Windows 10", "Silver"));
         
+        
+        filterNotebooks(notebooks);
     }
+
+    public static void filterNotebooks(Set<Notebook> notebooks) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите цифру, соответствующую необходимому критерию: \n1 - ОЗУ\n2 - Объем ЖД\n3 - Операционная система\n4 - Цвет");
+        int criterion = scanner.nextInt();
+        Map<Integer, Object> criteria = new HashMap<>();
+        
+        switch (criterion) {
+            case 1:
+                System.out.println("Введите минимальный объем ОЗУ:");
+                int minRam = scanner.nextInt();
+                criteria.put(1, minRam);
+                break;
+            case 2:
+                System.out.println("Введите минимальный объем ЖД:");
+                int minHdd = scanner.nextInt();
+                criteria.put(2, minHdd);
+                break;
+            case 3:
+                System.out.println("Введите операционную систему:");
+                String os = scanner.next();
+                criteria.put(3, os);
+                break;
+            case 4:
+                System.out.println("Введите цвет:");
+                String color = scanner.next();
+                criteria.put(4, color);
+                break;
+            default:
+                System.out.println("Некорректный ввод.");
+                return;
+        }
+        
+        for (Notebook notebook : notebooks) {
+            if (meetsCriteria(notebook, criteria)) {
+                System.out.println("Ноутбук " + notebook.getModel() + " соответствует критериям.");
+            }
+        }
+    }
+
+   
+    public static boolean meetsCriteria(Notebook notebook, Map<Integer, Object> criteria) {
+        if (criteria.containsKey(1) && notebook.getRamGb() < (int) criteria.get(1)) {
+            return false;
+        }
+        if (criteria.containsKey(2) && notebook.getHddGb() < (int) criteria.get(2)) {
+            return false;
+        }
+        if (criteria.containsKey(3) && !notebook.getOS().equals(criteria.get(3))) {
+            return false;
+        }
+        if (criteria.containsKey(4) && !notebook.getColor().equals(criteria.get(4))) {
+            return false;
+        }
+        return true;
+    }
+
+
 }
+
+
+
--- Notebook.java
+++ Notebook.java
@@ -1,5 +1,41 @@
+
+
 public class Notebook {
-    public static void main(String[] args) {
-        
+    // Поля класса Notebook
+    private String model;
+    private int ramGb;
+    private int hddGb;
+    private String os;
+    private String color;
+    
+    // Конструктор класса Notebook
+    public Notebook(String model, int ramGb, int hddGb, String os, String color) {
+        this.model = model;
+        this.ramGb = ramGb;
+        this.hddGb = hddGb;
+        this.os = os;
+        this.color = color;
+    }
+    
+    // Методы класса Notebook
+    // Геттеры для получения значений полей
+    public String getModel() {
+        return model;
+    }
+    
+    public int getRamGb() {
+        return ramGb;
+    }
+    
+    public int getHddGb() {
+        return hddGb;
+    }
+    
+    public String getOS() {
+        return os;
+    }
+    
+    public String getColor() {
+        return color;
     }
 }
